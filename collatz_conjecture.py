bd305db74ddf29b32c448daf72bae43dbcd5e6d3
mm13177 <115529791+mm13177@users.noreply.github.com>
2022-12-06 07:53:20 +0100

    Update collatz_conjecture.py

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,2 +1,13 @@
 def steps(number):
-    pass
+    if number(<=0):
+        raise ValueError ('Plase choose a positive number greater than 0')
+    else:
+        zaehler = 0
+        while (number !=1):
+            if((number%2)==0):
+                number = number//2
+                zaehler +=1
+            else:
+                number = (number*3)+1
+                zaehler +=1
+        return zaehler
