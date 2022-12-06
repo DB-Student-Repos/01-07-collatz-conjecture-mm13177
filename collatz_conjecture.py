2809823d1b67ef3375826f25fdef6545c40fbf61
Dominik Böhler <dominik.boehler@gmx.net>
2022-12-06 08:32:04 +0100

    Update collatz_conjecture.py
    Cleaned up syntax

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,14 +1,15 @@
 def steps(number):
+    """ Calculates Collatz Conjecture """
     
     if number <= 0:
         raise ValueError('Only positive numbers are allowed')
-    else:
-        zaehler = 0
-        while (number !=1):
-            if((number%2)==0):
-                number = number//2
-                zaehler +=1
-            else:
-                number = (number*3)+1
-                zaehler +=1
-        return zaehler
+   
+    cnt = 0
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+            cnt += 1
+        else:
+            number = number * 3 + 1
+            cnt += 1
+    return cnt
